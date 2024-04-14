57e71b27b19ee232c29d8a2d8631d52dcc003dd8
Tycho <tychosteur@gmail.com>
2024-04-14 14:02:01 +0200

    separated the file mapping functions to a separate file

---
+++ src/file_mapping.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "file_mapping.h"
+
+off_t get_file_size(int fd) {
+    off_t original_offset = lseek(fd, 0, SEEK_CUR);
+    if (original_offset == (off_t)-1) {
+        return (-1);
+    }
+    off_t file_size = lseek(fd, 0, SEEK_END);
+    (void)lseek(fd, original_offset, SEEK_SET);
+    if (file_size == (off_t)-1) {
+        return (-1);
+    }
+    return (file_size);
+}
+
+int map_file(int fd, int prot, int flags, void** map, size_t* map_size) {
+    off_t file_size = get_file_size(fd);
+    if (file_size == (off_t)-1) {
+        return (-1);
+    }
+
+    *map_size = file_size;
+    *map = mmap(NULL, *map_size, prot, flags, fd, 0);
+    if (*map == MAP_FAILED) {
+        return (-1);
+    }
+    return (0);
+}
---
+++ src/file_mapping.h
@@ -0,0 +1,10 @@
+#ifndef FILE_MAPPING_H
+# define FILE_MAPPING_H
+
+#include <unistd.h>
+
+off_t get_file_size(int fd);
+
+int map_file(int fd, int prot, int flags, void** map, size_t* map_size);
+
+#endif
