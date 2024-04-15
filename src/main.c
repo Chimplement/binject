cad4509498f10f2117c3e76b22c543e1d1af34d9
Tycho <tychosteur@gmail.com>
2024-04-15 22:23:44 +0200

    payloads are now loaded from a file

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <stdlib.h>
 #include <string.h>
 
 #include <sys/mman.h>
@@ -14,38 +15,72 @@
 #include "file_map.h"
 #include "elf_map.h"
 
-unsigned char shell_code[] = {
-    0x52,                                       // push   %rdx
-    0xb8, 0x01, 0x00, 0x00, 0x00,               // mov    $0x1,%eax
-    0xbf, 0x01, 0x00, 0x00, 0x00,               // mov    $0x1,%edi
-    0x48, 0x8d, 0x35, 0x11, 0x00, 0x00, 0x00,   // lea    0x11(%rip),%rsi
-    0xba, 0x0e, 0x00, 0x00, 0x00,               // mov    $0xe,%edx
-    0x0f, 0x05,                                 // syscall
-    0x5a,                                       // pop    %rdx
-    0x48, 0x8d, 0x05, 0xff, 0xff, 0xff, 0xff,   // lea    -0x1(%rip),%rax
-    0xff, 0xe0,                                 // jmp    *%rax
-    'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\n',
+// unsigned char shell_code[] = {
+//     0x52,                                       // push   %rdx
+//     0xb8, 0x01, 0x00, 0x00, 0x00,               // mov    $0x1,%eax
+//     0xbf, 0x01, 0x00, 0x00, 0x00,               // mov    $0x1,%edi
+//     0x48, 0x8d, 0x35, 0x11, 0x00, 0x00, 0x00,   // lea    0x11(%rip),%rsi
+//     0xba, 0x0e, 0x00, 0x00, 0x00,               // mov    $0xe,%edx
+//     0x0f, 0x05,                                 // syscall
+//     0x5a,                                       // pop    %rdx
+//     0x48, 0x8d, 0x05, 0xff, 0xff, 0xff, 0xff,   // lea    -0x1(%rip),%rax
+//     0xff, 0xe0,                                 // jmp    *%rax
+//     'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\n',
+// };
+
+uint8_t payload_prefix[] = {
+    0x48, 0x8d, 0x05, 0xff, 0xff, 0xff, 0xff,   // lea  -0x1(%rip),%rax
+    0x50,                                       // push %rax
 };
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) return (1);
+    if (argc < 3) return (1);
 
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
+    int target_fd = open(argv[1], O_RDONLY);
+    if (target_fd == -1) {
         return (1);
     }
 
     unsigned char* map;
     size_t map_size;
-    if (map_file(fd, PROT_READ | PROT_WRITE, MAP_PRIVATE, (void**)&map, &map_size) == -1) {
-        close(fd);
+    if (map_file(target_fd, PROT_READ | PROT_WRITE, MAP_PRIVATE, (void**)&map, &map_size) == -1) {
+        close(target_fd);
         return (1);
     }
-    close(fd);
+    close(target_fd);
+
+    int payload_fd = open(argv[2], O_RDONLY);
+    if (payload_fd == -1) {
+        (void)munmap(map, map_size);
+        return (1);
+    }
+
+    off_t payload_size = get_file_size(payload_fd);
+    if (payload_size == (off_t)-1) {
+        (void)munmap(map, map_size);
+        close(payload_fd);
+        return (1);
+    }
+
+    uint8_t* payload = malloc(payload_size);
+    if (payload == NULL) {
+        (void)munmap(map, map_size);
+        close(payload_fd);
+        return (1);
+    }
+    if (read(payload_fd, payload, payload_size) == -1) {
+        (void)munmap(map, map_size);
+        close(payload_fd);
+        free(payload);
+        return (1);
+    }
+    (void)close(payload_fd);
+
 
     elf64_map_t elf_map = map_elf64(map, map_size);
     if (elf_map.start == NULL) {
         (void)munmap(map, map_size);
+        free(payload);
         return (1);
     }
 
@@ -54,32 +89,35 @@ int main(int argc, char* argv[]) {
     Elf64_Phdr* executable_header = find_program_header(elf_map, PT_LOAD, PF_R | PF_X);
     if (executable_header == NULL) {
         (void)munmap(map, map_size);
+        free(payload);
         return (1);
     }
 
     uint8_t* code_cave = (uint8_t*)find_code_cave(
         elf_map.start + executable_header->p_offset,
-        sizeof(shell_code),
-        executable_header->p_filesz + sizeof(shell_code) + 16,
+        sizeof(payload_prefix) + payload_size,
+        executable_header->p_filesz + sizeof(payload_prefix) + payload_size + 16,
         8
     );
 
     if (code_cave == NULL) {
         (void)munmap(map, map_size);
+        free(payload);
         return (1);
     }
 
     printf("code_cave: %p\n", (void*)(code_cave - elf_map.start));
 
-    int offset = elf_map.header->e_entry - (code_cave - elf_map.start) - 33;
+    int offset = elf_map.header->e_entry - (code_cave - elf_map.start) - 7;
     printf("jmp offset: %x (%i)\n", offset, offset);
 
-    memcpy(shell_code + 29, &offset, 4);
-    memcpy(code_cave, shell_code, sizeof(shell_code));
+    memcpy(payload_prefix + 3, &offset, 4);
+    memcpy(code_cave, payload_prefix, sizeof(payload_prefix));
+    memcpy(code_cave + sizeof(payload_prefix), payload, payload_size);
 
     executable_header->p_filesz = umax(
         executable_header->p_filesz,
-        (code_cave - elf_map.start) + sizeof(shell_code) - executable_header->p_offset
+        (code_cave - elf_map.start) + sizeof(payload_prefix) + payload_size - executable_header->p_offset
     );
     executable_header->p_memsz = umax(
         executable_header->p_memsz,
@@ -87,13 +125,14 @@ int main(int argc, char* argv[]) {
     );
     elf_map.header->e_entry = code_cave - elf_map.start;
 
-    fd = open("test", O_CREAT | O_WRONLY, 0744);
-    if (fd != -1)
+    int out_fd = open("injected", O_CREAT | O_WRONLY, 0744);
+    if (out_fd != -1)
     {
-        write(fd, map, map_size);
-        close(fd);
+        write(out_fd, map, map_size);
+        close(out_fd);
     }
 
     (void)munmap(map, map_size);
+    free(payload);
     return (0);
 }
