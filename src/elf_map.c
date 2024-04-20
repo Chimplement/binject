a0615979c3f15cff4c2b62ab76d7027e2f9c7adc
Tycho <tychosteur@gmail.com>
2024-04-20 12:39:24 +0200

    added the respective 32 bit versions of the elf mapping functions

--- src/elf_map.c
+++ src/elf_map.c
@@ -16,6 +16,28 @@ uint8_t elf_class(void* start, size_t size) {
     return (((uint8_t*)start)[EI_CLASS]);
 }
 
+elf32_map_t map_elf32(void* start, size_t size) {
+    elf32_map_t elf_map;
+
+    elf_map.start = start;
+
+    if (size < 52) {
+        return ((elf32_map_t){.start = NULL});
+    }
+
+    if (elf_map.header->e_phoff + elf_map.header->e_phnum * elf_map.header->e_phentsize > size) {
+        return ((elf32_map_t){.start = NULL});
+    }
+    if (elf_map.header->e_shoff + elf_map.header->e_shnum * elf_map.header->e_shentsize > size) {
+        return ((elf32_map_t){.start = NULL});
+    }
+
+    elf_map.program_headers = (Elf32_Phdr*)(elf_map.start + elf_map.header->e_phoff);
+    elf_map.section_headers = (Elf32_Shdr*)(elf_map.start + elf_map.header->e_shoff);
+
+    return (elf_map);
+}
+
 elf64_map_t map_elf64(void* start, size_t size) {
     elf64_map_t elf_map;
 
@@ -36,6 +58,18 @@ elf64_map_t map_elf64(void* start, size_t size) {
     elf_map.section_headers = (Elf64_Shdr*)(elf_map.start + elf_map.header->e_shoff);
 
     return (elf_map);
+}
+
+Elf32_Phdr* find_program_header32(elf32_map_t elf_map, Elf32_Word type, Elf32_Word flags) {
+    Elf32_Phdr* ph;
+
+    for (unsigned int i = 0; i < elf_map.header->e_phnum; i++) {
+        ph = elf_map.program_headers + i;
+        if (ph->p_type == type && (ph->p_flags & flags) == flags) {
+            return (ph);
+        }
+    }
+    return (NULL);
 }
 
 Elf64_Phdr* find_program_header64(elf64_map_t elf_map, Elf64_Word type, Elf64_Word flags) {
--- src/elf_map.h
+++ src/elf_map.h
@@ -12,6 +12,17 @@ typedef struct
     union
     {
         uint8_t* start;
+        Elf32_Ehdr* header;
+    };
+    Elf32_Phdr* program_headers;
+    Elf32_Shdr* section_headers;
+}   elf32_map_t;
+
+typedef struct
+{
+    union
+    {
+        uint8_t* start;
         Elf64_Ehdr* header;
     };
     Elf64_Phdr* program_headers;
@@ -21,8 +32,10 @@ typedef struct
 bool is_elf(void* start, size_t size);
 uint8_t elf_class(void* start, size_t size);
 
+elf32_map_t map_elf32(void* start, size_t size);
 elf64_map_t map_elf64(void* start, size_t size);
 
+Elf32_Phdr* find_program_header32(elf32_map_t elf_map, Elf32_Word type, Elf32_Word flags);
 Elf64_Phdr* find_program_header64(elf64_map_t elf_map, Elf64_Word type, Elf64_Word flags);
 
 #endif
